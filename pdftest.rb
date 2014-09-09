383d00a48b81b7e36bb58b67aa9ffeb3bd0480ed
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-09-09 09:16:00 +0900

    new file:   .gitignore 	new file:   pdftest.rb

---
+++ pdftest.rb
@@ -0,0 +1,18 @@
+require 'pdf/reader'
+
+File.open("./tmp/test.pdf", "rb") do |io|
+
+  reader = PDF::Reader.new(io)
+
+  puts reader.pdf_version
+  puts reader.info
+  puts reader.page_count
+
+  reader.pages.each do |page|
+    puts page.text
+    break
+
+  end
+
+end
+
