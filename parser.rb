06f410c83b70e93651cc8f84ece5afaaf167e525
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-09-11 22:36:15 +0900

    modified:   parser.rb

--- parser.rb
+++ parser.rb
@@ -2,24 +2,20 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-url = 'http://matome.naver.jp/tech'
+url = 'http://yahoo.co.jp'
 
 charset = nil
 html = open(url) do |f|
-charset = f.charset # 文字種別を取得
-f.read # htmlを読み込んで変数htmlに渡す
+charset = f.charset 
+f.read
 end
 
-# htmlをパース(解析)してオブジェクトを作成
 doc = Nokogiri::HTML.parse(html, nil, charset)
 
   doc.xpath('//li[@class="mdTopMTMList01Item"]').each do |node|
-# tilte
+
   p node.css('h3').inner_text
-
-# 記事のサムネイル画像
   p node.css('img').attribute('src').value
-
-# 記事のサムネイル画像
   p node.css('a').attribute('href').value
+
   end
