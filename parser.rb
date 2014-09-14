97d258462e0216deb2cdd3196e2fb4a3d15567a5
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-09-15 01:05:11 +0900

    modified:   parser.rb

--- parser.rb
+++ parser.rb
@@ -4,8 +4,13 @@ require 'open-uri'
 
 # irb
 
-url = 'http://yahoo.co.jp'
 charset = nil
+url = ""
+
+ARGV.each do |arg|
+  url = arg
+end
+
 
 html = open(url) do |f|
   charset = f.charset 
@@ -14,6 +19,22 @@ end
 
 doc = Nokogiri::HTML.parse(html, nil, charset)
 
-p doc.title
+
+# frame
+frame_url = File.dirname(url) + "/" + doc.css('frame')[1]['src']
+
+
+html = open(frame_url) do |f|
+  charset = f.charset 
+  f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+doc.css('td.evennew000 a').each do |link|
+
+  p link.content
+  p link['href']
+
+end
 
 
