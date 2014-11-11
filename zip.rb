decbda7200e604d0ad5bb23e3990c16cdd8ada79
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2014-11-11 09:33:15 +0900

    modified:   zip.rb

--- zip.rb
+++ zip.rb
@@ -4,18 +4,18 @@ require 'pathname'
 
 Zip::File.open('tmp/091220141028066892.zip') do |zip_file|
   # Handle entries one by one
-  zip_file.each do |entry|
-    # Extract to file/directory/symlink
-    puts "Extracting #{entry.name}"
-    #entry.extract(entry.name)
-    # Read into memory
-    content = entry.get_input_stream.read
-    #p content
-  end
+  #zip_file.each do |entry|
+  #  # Extract to file/directory/symlink
+  #  puts "Extracting #{entry.name}"
+  #  #entry.extract(entry.name)
+  #  # Read into memory
+  #  content = entry.get_input_stream.read
+  #  #p content
+  #end
 
   # Find specific entry
-  #entry = zip_file.glob('*.csv').first
-  #puts entry.get_input_stream.read
+  entry = zip_file.glob('*.htm').first
+  p entry.get_input_stream.read
 
 end
 
