1cb8eff6a0b128fb18d02c17c6b33a8535e5f745
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-01-26 08:17:21 +0900

    modified:   map.rb

--- map.rb
+++ map.rb
@@ -5,6 +5,9 @@ p [1, 2, 3, 4, 5, 6].collect{|x| x ** x}
 h = {aa: 3, bb: 4}
 p h.map{|x| x * 2}
 
+p [{name: "taro", age: 8}, {name: "jiro", age: 7}].map{|x| x[:age] * 2}
+
 for i in 2007..2015 do
   p i
 end
+
