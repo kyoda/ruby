f72f349833c678f98115365763a88535dd352dcb
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-01-27 08:58:56 +0900

    modified:   map.rb

--- map.rb
+++ map.rb
@@ -3,9 +3,15 @@ p [1, 2, 3, 4, 5, 6].map{|x| x * x}
 p [1, 2, 3, 4, 5, 6].collect{|x| x ** x}
 
 h = {aa: 3, bb: 4}
-p h.map{|x| x * 2}
+p h.map{|x| x * 2} # hash => array ?
+
 
 p [{name: "taro", age: 8}, {name: "jiro", age: 7}].map{|x| x[:age] * 2}
+p [[1, 2], [3, 4]].map{|x| x * 2}
+p [5, 6] * 2
+
+hh = {cc: 3}
+p hh
 
 for i in 2007..2015 do
   p i
