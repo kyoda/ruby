6fe154de1b553b150d32fc4079df02d04cdc437f
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-02-23 09:13:44 +0900

    modified:   time.rb

--- time.rb
+++ time.rb
@@ -1,5 +1,6 @@
 
-now_obj = Time.strptime("2014-08-08", "%Y-%m-%d")
-now_obj += 24 * 60 * 60
-p now_obj.strftime("%Y-%m-%d")
+day_obj = Time.strptime("2014-08-08", "%Y-%m-%d")
+# tomorrow
+day_obj += 24 * 60 * 60
+p day_obj.strftime("%Y-%m-%d")
 
