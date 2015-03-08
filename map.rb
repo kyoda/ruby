3e43fd555311665e71c957d6fc5ac545e941b74b
Shigehisa KYODA <kyoda@ns.ie.u-ryukyu.ac.jp>
2015-03-09 08:59:36 +0900

    modified:   map.rb

--- map.rb
+++ map.rb
@@ -5,8 +5,9 @@ p [1, 2, 3, 4, 5, 6].collect{|x| x ** x}
 h = {aa: 3, bb: 4}
 p h.map{|x| x * 2} # hash => array ?
 
-
-p [{name: "taro", age: 8}, {name: "jiro", age: 7}].map{|x| x[:age] * 2}
+user = [{name: "taro", age: 8}, {name: "jiro", age: 7}]
+p user.map!{|x| x[:age] * 2}
+p user
 p [[1, 2], [3, 4]].map{|x| x * 2}
 p [5, 6] * 2
 
